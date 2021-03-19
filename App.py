e62ea5c27d12f15254d01caf988dd4dde9510381
porosh2002 <jamilkashem2002@gmail.com>
2021-03-20 02:24:27 +0600

    Converting to GreyScale and Show GreyScale Image

--- App.py
+++ App.py
@@ -1,3 +1,18 @@
 import cv2
 
+# Choose an Image to Detect Face
+img = cv2.imread('./image.jpg')
+
+# Convert Image into GreyScale
+greyScaled_Img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Show Image 
+cv2.imshow('Face Detection',greyScaled_Img)
+
+# Waition for an Action
+cv2.waitKey()
+  
+
+
+
 print("Code Compiled Successfully")
