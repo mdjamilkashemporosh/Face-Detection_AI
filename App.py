c5abe1da8f95bf92ef5a968521410f0213341c35
porosh2002 <jamilkashem2002@gmail.com>
2021-03-21 00:21:53 +0600

    Detecting Faces

--- App.py
+++ App.py
@@ -1,18 +1,23 @@
 import cv2
 
+Trained_Face_Data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 # Choose an Image to Detect Face
 img = cv2.imread('./image.jpg')
 
 # Convert Image into GreyScale
 greyScaled_Img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  
+# Detect Faces
+Face_Cordinates = Trained_Face_Data.detectMultiScale(greyScaled_Img)
+
+# Draw Rectangles Around the Faces 
+for(x,y,w,h) in Face_Cordinates : 
+    cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
 
 # Show Image 
-cv2.imshow('Face Detection',greyScaled_Img)
+cv2.imshow('Face Detection',img)
 
 # Waition for an Action
 cv2.waitKey()
-  
-
-
 
 print("Code Compiled Successfully")
